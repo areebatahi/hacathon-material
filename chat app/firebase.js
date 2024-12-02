32a8ccf7fb970aaf194ed45ddba1604224082928
areebatahir <areebatahirarain@gmail.com>
2024-12-03 02:59:14 +0500

    complete c

---
+++ chat app/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCpEubZ_9Uyq8sS_Rs45MnkKuuCrDP0mCc",
+    authDomain: "fir-project-4f9d5.firebaseapp.com",
+    projectId: "fir-project-4f9d5",
+    storageBucket: "fir-project-4f9d5.firebasestorage.app",
+    messagingSenderId: "999036777326",
+    appId: "1:999036777326:web:fe87056fbcfdc838bd477f",
+    measurementId: "G-3JGHM3XL30"
+};
+
+const app = initializeApp(firebaseConfig);
